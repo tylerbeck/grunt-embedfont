0a352ead63193d113aeb674bce174862665d068e
Tyler Beck <tyler@tylerbeck.com>
2015-02-25 23:35:58 -0500

    adding grunt bump

--- Gruntfile.js
+++ Gruntfile.js
@@ -129,7 +129,27 @@ module.exports = function(grunt) {
     // Unit tests.
     nodeunit: {
       tests: ['test/*_test.js']
-    }
+    },
+
+	  bump: {
+		  options: {
+			  files: ['package.json'],
+			  updateConfigs: [],
+			  commit: true,
+			  commitMessage: 'Release v%VERSION%',
+			  commitFiles: ['package.json'],
+			  createTag: true,
+			  tagName: 'v%VERSION%',
+			  tagMessage: 'Version %VERSION%',
+			  push: false,
+			  pushTo: 'upstream',
+			  gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
+			  globalReplace: false,
+			  prereleaseName: false,
+			  regExp: false
+		  }
+	  }
+
 
   });
 
